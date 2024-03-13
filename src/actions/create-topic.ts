acd47dc539a7cb8e84ee1dd65b16810a8a18f5c5
Levon Aslanyan <aslanyan2012@mail.ru>
2024-03-14 00:19:09 +0400

    finish the form, useFormstatus

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,79 @@
 "use server";
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+import paths from "@/paths";
+import { resolve } from "path";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase ot dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 
-  console.log(name, description, "===");
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  // await new Promise((resolve) => setTimeout(resolve, 2000)); //* time for loading 
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this!* "],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
